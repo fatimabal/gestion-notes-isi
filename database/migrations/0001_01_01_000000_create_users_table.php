5538b10dc52abe2f7b7220a43b233234d5992819
Fatimabal <fatimabal298@gmail.com>
2026-06-25 12:37:45 -1200

    Ajout des donnees dans la table user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Enseignant;
+use App\Models\Scolarite;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,10 +15,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nom');
+            $table->string('prenom');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('telephone');
+            $table->enum ('role',['etudiant','enseignant','scolarite','parent']);
             $table->rememberToken();
             $table->timestamps();
         });
