89b9b7bff8b8e2deda5d47f5914577b96f13c546
Fatimabal <fatimabal298@gmail.com>
2026-07-01 00:57:19 +0000

    endpoints register et login fonctionnels + test postman

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
-
+use Illuminate\Support\Facades\Auth;
 class AuthController extends Controller
 {
     public function register(Request $request)
@@ -35,4 +35,21 @@ class AuthController extends Controller
             'token' => $token
         ], 201);
     }
+    public function login(Request $request)
+    {
+     $request->validate([
+        'email'=>'required|email',
+        'password' => 'required|min:6'
+
+     ]);
+     if(!Auth::attempt(['email'=> $request->email,'password'=>$request->password])){
+        return response()->json(['message'=>'Identifiants incorrects'],401);
+     }
+     $user=Auth::user();
+     $token=$user->createToken('auth_token')->plainTextToken;
+return response()->json([
+    'user' => $user,
+    'token' => $token
+],   200);
+ }
 }
